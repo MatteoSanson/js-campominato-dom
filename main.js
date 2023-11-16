2bf3b1d52d75140739cd4841663e1cc80252de86
MatteoSanson <matteo.sanson@gmail.com>
2023-11-16 16:06:38 +0100

    feat: selezione livello

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
 'use strict';
 
-function elementoCella(tag, className, content) {
+function elementoCella(tag, classNames, content) {
     const element = document.createElement(tag);
-    element.classList.add(className);
+    classNames.forEach(className => element.classList.add(className));
     element.append(content);
 
     return element;
 }
 
 function aggiungiEventoClick(element, clickedCells) {
-    element.addEventListener('click', function() {
+    element.addEventListener('click', function () {
         if (!element.classList.contains('clicked')) {
             element.classList.add('clicked');
             const cellNumber = element.textContent;
@@ -25,19 +25,46 @@ let clickedCells = [];
 const griglia = document.querySelector('.griglia');
 const playButton = document.querySelector('.play');
 const startMessage = document.querySelector('.start-message');
+const levelSelect = document.getElementById('level');
 
-playButton.addEventListener('click', function() {
+playButton.addEventListener('click', function () {
     startMessage.style.display = 'none';
 
     griglia.innerHTML = '';
     grigliaGenerata = false;
     clickedCells = [];
 
+    const selectedLevel = levelSelect.value;
+
+    let cellClass;
+    let targetCellCount;
+
+    switch (selectedLevel) {
+        case 'livello1':
+            cellClass = 'cell-1';
+            targetCellCount = 100;
+            break;
+        case 'livello2':
+            cellClass = 'cell-2';
+            targetCellCount = 81;
+            break;
+        case 'livello3':
+            cellClass = 'cell-3';
+            targetCellCount = 49;
+            break;
+        default:
+            cellClass = 'cell-1';
+            targetCellCount = 100;
+            break;
+    }
+
     if (!grigliaGenerata) {
-        for (let i = 1; i <= 100; i++) {
-            const mioElemento = elementoCella('div', 'cell', i);
+        let i = 1;
+        while (document.querySelectorAll(`.${cellClass}`).length < targetCellCount) {
+            const mioElemento = elementoCella('div', ['cell', cellClass], i);
             aggiungiEventoClick(mioElemento, clickedCells);
             griglia.append(mioElemento);
+            i++;
         }
         grigliaGenerata = true;
     }
